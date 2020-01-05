c703afd9940ed7727b5966bb33088b1665647486
Joshua Coleman <procoleman77@outlook.com>
2020-01-04 20:53:14 -0500

    Updates

---
+++ precache-manifest.29a794951e7e3b8ac9cfdf746e521865.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c57d26d9e76aabdeb47241b3bb60c19",
+    "url": "/React-Game/index.html"
+  },
+  {
+    "revision": "97221bdd9e48c3621ba3",
+    "url": "/React-Game/static/css/main.49725c92.chunk.css"
+  },
+  {
+    "revision": "cfe6f670c33a0df258f0",
+    "url": "/React-Game/static/js/2.543d28a4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-Game/static/js/2.543d28a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "97221bdd9e48c3621ba3",
+    "url": "/React-Game/static/js/main.390a60f0.chunk.js"
+  },
+  {
+    "revision": "67c08b85a3322315b6b7",
+    "url": "/React-Game/static/js/runtime-main.920ee38f.js"
+  }
+]);
